bb74817fa7735fe30739e71674a52e87796a3ee7
rhp <1919324356@qq.com>
2022-08-07 10:42:12 +0800

    hot-fix commit

--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("git-test 01");
         System.out.println("git-test 02");
         System.out.println("git-test 03");
+        System.out.println("git-test 04");
     }
 }
