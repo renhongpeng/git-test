dc651c6b0d92b9ac83a4c33d8e31d8d6fd79dc6b
renhongpeng <110729950+renhongpeng@users.noreply.github.com>
2022-08-07 11:20:07 +0800

    pull commit

--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("master test");
         System.out.println("hot-fix test02");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
