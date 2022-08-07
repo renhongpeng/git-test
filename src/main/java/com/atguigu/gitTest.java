4b0cf07b1ba2a2b0fc4cf13289838cd9c322091b
rhp <1919324356@qq.com>
2022-08-07 10:46:33 +0800

    master commit02

--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("git-test 02");
         System.out.println("git-test 03");
         System.out.println("git-test 04");
+        System.out.println("master test");
     }
 }
